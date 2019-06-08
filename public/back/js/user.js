688fd3b43d4768f97a9adf6aa91f2e5b8f956d43
corlme <342793579@qq.com>
2019-06-08 17:03:15 +0800

    用户管理-分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,18 +1,47 @@
 $(function(){
+
+    var currentPage = 1; //当前页
+    var pageSize = 5;    //每页条数
    // 一进入页面就发送ajax请求 获取用户列表数据，通过模板引擎渲染
-    $.ajax({
-        type:"get",
-        url:"/user/queryUser",
-        data:{
-            page:1,
-            pageSize:5
-        },
-        dataType:"json",
-        success:function(info){
-            console.log(info);
-            var htmlStr = template("tpl",info);
-            $("tbody").html(htmlStr);
-        }
-    })
+    render();
+    function render(){
+        $.ajax({
+            type:"get",
+            url:"/user/queryUser",
+            data:{
+                page:currentPage,
+                pageSize:pageSize
+            },
+            dataType:"json",
+            success:function(info){
+                console.log(info);
+                var htmlStr = template("tpl",info);
+                $("tbody").html(htmlStr);
+
+                // 分页初始化
+                $("#paginator").bootstrapPaginator({
+                    // 指定bootstrap的版本
+                    bootstrapMajorVersion: 3,
+                    // 总页数
+                    totalPages: Math.ceil( info.total / info.size ),
+                    // 当前页
+                    currentPage: info.page,
+                    onPageClicked: function(a,b,c,page){
+                        console.log(page);
+                        // 通过page获取点击页码
+                       // 更新当前页 （把page赋值给当前页）
+                       currentPage=page;
+                       // 页面重新渲染
+                        render();
+                    }
+                });
+
+            }
+
+
+        });
+    }
+
+
 
 });
