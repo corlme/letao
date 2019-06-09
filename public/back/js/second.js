5ab2858e78edcd3b306fe49ac99c2e5adc7feb42
corlme <342793579@qq.com>
2019-06-09 09:36:03 +0800

    二级分类-设置隐藏域进行提交

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -18,7 +18,7 @@ $(function(){
                     var htmlStr = template("secondTpl",info);
                     $("tbody").html(htmlStr);
 
-                // 2.分页初始化
+                // 分页初始化
                 $("#paginator").bootstrapPaginator({
                     // 指定版本
                     bootstrapMajorVersion:3,
@@ -40,5 +40,58 @@ $(function(){
         })
     }
 
+    // 2.点击分类按钮 显示模态框
+     $("#addBtn").click(function(){
+         $("#addModal").modal("show");
+
+         // 一点击分类按钮就发送ajax请求 获取数据 通过模板引擎渲染
+         // 通过设置 page=1  pageSize=100 来模拟数据
+         $.ajax({
+             type:"get",
+             url:"/category/queryTopCategoryPaging",
+             data: {
+                 page:1,
+                 pageSize:100
+             },
+             dataType:"json",
+             success:function(info){
+                 console.log(info);
+                 var htmlStr = template("dropdownTpl", info);
+                 $(".dropdown-menu").html(htmlStr);
+             }
+         })
+     })
+
+    // 3.通过事件委托 给 dropdown-menu 下面所有的 a 绑定点击事件
+    $(".dropdown-menu").on("click","a", function(){
+        // 获取 a 的文本
+        var txt = $(this).text();
+        // 把这个文本赋值给 ul
+         $("#dropdownText").html(txt);
+
+         // 获取选中的一级分类中 a 的 id
+        var id = $(this).data( 'id' );
+         // 把id 赋值给input框（val）
+        $('[name="categoryId"]').val(id);
+    });
+
+    // 4.进行文件上传初始化
+    $("#fileupload").fileupload({
+        // 配置返回的数据格式
+        dataType:"json",
+        // 配置图片上传完成后会调用的 done 回调函数
+        done:function( e, data ){
+            var imgUrl = data.result.picAddr;
+            $("#imgBox img" ).attr("src",imgUrl);
+
+            $('[name="brandLogo"]').val( imgUrl );
+
+        }
+    });
+
+    // 文件上传思路整理
+   // 1，引包
+   //  2 准备结构 name  data-url
+   //  3 进行文件上传初始化 配置 done 回调函数
 
 });
