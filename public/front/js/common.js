7ec4f27d7ee29c3f19823a66c84e0dd45a7ccdc0
corlme <342793579@qq.com>
2019-06-10 22:03:43 +0800

    分类页-静态页面完成

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -1,6 +1,6 @@
 
 
-// mui 插件 实现区域滚动初始化
+// mui 插件 给所有页面实现区域滚动初始化
 mui('.mui-scroll-wrapper').scroll({
     indicators: false, //是否显示滚动条
     deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
